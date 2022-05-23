29feffeb21bc068eb68620d44adc4175fc78c8f5
langzhou <1099133317@qq.com>
2022-05-23 23:25:19 +0800

    feat: 新增 searchbox

--- js/index.js
+++ js/index.js
@@ -1,17 +1,58 @@
-import InlineComment from "./libs/InlineComment.js";
-import LinkCard from "./libs/LinkCard.js";
-import config from "./config.js";
+import InlineComment from "./libs/InlineComment.js"
+import LinkCard from "./libs/LinkCard.js"
+import SearchBox from "./libs/SearchBox.js"
+import config from "./config.js"
 
 class SiyuanPlugin {
   constructor() {
-    this.themeName = this.getThemeName();
+    this.themeName = this.getThemeName()
     if (this.themeName) {
       // this.appendStyleSheet()
-      this.inlineComment = new InlineComment();
-      this.linkCard = new LinkCard();
-      this.domWatcher();
-      this.handleEvents();
-      setTimeout(() => this.appendToolbarBtn(), 1000); //添加 toolbar 评论按钮
+      this.inlineComment = new InlineComment()
+      this.linkCard = new LinkCard()
+      this.searchBox = new SearchBox()
+      this.domWatcher()
+      this.handleEvents()
+      setTimeout(() => this.appendToolbarBtn(), 1000) //添加 toolbar 评论按钮
+    }
+  }
+
+  /* 快捷键注册 */
+  shortcutKey(e) {
+    // shift+alt+t for test
+    // if(e.shiftKey && e.altKey && e.code =='KeyT'){
+    //   e.preventDefault()
+    //   e.stopPropagation()
+    //   // 一键切换卡片视图
+    //   this.cardLayout = !this.cardLayout
+    //   let protyle = document.querySelector('.fn__flex-1.protyle:not(.fn__none) .protyle-wysiwyg.protyle-wysiwyg--attr') //需要获取到当前正在编辑的 protyle
+
+    //   if(this.cardLayout){
+    //     protyle.classList.add('card')
+    //   }else{
+    //     protyle.classList.remove('card')
+    //   }
+
+    // }
+
+    // shift+alt+s 弹出图片分享
+    // if(e.shiftKey && e.altKey && e.code =='KeyS'){
+    //   e.preventDefault()
+    //   e.stopPropagation()
+    //   if(!this.imageShare){
+    //     this.imageShare = new ImageShare()
+    //   }
+    //   this.imageShare.showBox()
+    // }
+
+    // shift+alt+p 弹出快捷搜索框
+    if (e.shiftKey && e.altKey && e.code == "KeyP") {
+      e.preventDefault()
+      e.stopPropagation()
+      if (this.searchBox) {
+        this.searchBox.create() //创建搜索框
+        this.searchBox.showBox() //展示搜索框
+      }
     }
   }
 
@@ -19,56 +60,61 @@ class SiyuanPlugin {
   handleEvents() {
     // 按键按下事件
     window.addEventListener("keydown", (e) => {
-      // this.shortcutKey(e)
-      if (this.inlineComment) this.inlineComment.handleKeyDown(e);
-    });
+      this.shortcutKey(e)
+      if (this.inlineComment) this.inlineComment.handleKeyDown(e)
+      if (this.searchBox) this.searchBox.handleKeyDown(e)
+    })
 
     // 输入防抖
     // window.addEventListener('keyup',lodash.debounce(e =>{
     //   if(this.searchBox) this.searchBox.handleInput(e)
     // },800))
 
-    // 按键弹起事件
     // window.addEventListener('keyup',e =>{
-    //   if(this.searchBox) this.searchBox.actionTrigger(e)
-    // })
+    //     if(this.searchBox) this.searchBox.handleInput(e)
+    //   })
+
+    // 按键弹起事件
+    window.addEventListener("keyup", (e) => {
+      if (this.searchBox) this.searchBox.actionTrigger(e)
+    })
 
     // 鼠标单击事件
     window.addEventListener("click", (e) => {
-      if (this.inlineComment) this.inlineComment.showBox(e);
-    });
+      if (this.inlineComment) this.inlineComment.showBox(e)
+    })
 
     // 鼠标松开事件
     window.addEventListener("mouseup", (e) => {
-      if (this.inlineComment) this.inlineComment.handleSelectionEvent(e);
+      if (this.inlineComment) this.inlineComment.handleSelectionEvent(e)
       // if (this.linkCard) this.linkCard.handleSelectionEvent(e);
-    });
+    })
   }
 
   /* 检测 dom 变动，用于动态插入元素 */
   domWatcher() {
     var targetNode = document.querySelector(
       ".layout__center.fn__flex.fn__flex-1"
-    );
+    )
     if (!targetNode) {
       setTimeout(() => {
-        this.domWatcher();
-      }, 300);
+        this.domWatcher()
+      }, 300)
     } else {
-      const config = { attributes: false, childList: true, subtree: true };
+      const config = { attributes: false, childList: true, subtree: true }
       const callback = (mutationsList, observer) => {
         for (let mutation of mutationsList) {
           if (mutation.type === "childList") {
-            this.childListChangedHook(mutation);
+            this.childListChangedHook(mutation)
           } else if (mutation.type === "attributes") {
             console.log(
               "The " + mutation.attributeName + " attribute was modified."
-            );
+            )
           }
         }
-      };
-      const observer = new MutationObserver(callback);
-      observer.observe(targetNode, config);
+      }
+      const observer = new MutationObserver(callback)
+      observer.observe(targetNode, config)
       // observer.disconnect();
     }
   }
@@ -77,94 +123,94 @@ class SiyuanPlugin {
   childListChangedHook(mutation) {
     // 添加工具条按钮
     if (mutation.addedNodes) {
-      let node = mutation.addedNodes.item(0);
+      let node = mutation.addedNodes.item(0)
       // 新增 protyle 节点，即判断为打开了新文档
       if (node && node.className == "fn__flex-1 protyle") {
         // 因为 dom 树可能没有完全加载，需要延迟处理
         setTimeout(() => {
-          this.appendToolbarBtn();
-        }, 1000);
+          this.appendToolbarBtn()
+        }, 1000)
       }
     }
 
-    if (this.inlineComment) this.inlineComment.domWatcher(mutation);
-    if (this.linkCard) this.linkCard.domWatcher(mutation);
+    if (this.inlineComment) this.inlineComment.domWatcher(mutation)
+    if (this.linkCard) this.linkCard.domWatcher(mutation)
   }
 
   //添加工具条按钮
   appendToolbarBtn() {
     // 初始化时找到所有 protyle-toolbar
-    let toolbars = document.querySelectorAll(".protyle-toolbar");
+    let toolbars = document.querySelectorAll(".protyle-toolbar")
     const data = {
       type: "test",
       label: "添加评论",
       divider: false,
       icon: "comment",
       func: (e) => {
-        console.log(e);
+        console.log(e)
       },
-    };
+    }
 
     if (toolbars) {
       toolbars.forEach((item, index, node) => {
         // console.log(item);
         if (!item.querySelector(`[data-type="${data.type}"]`)) {
-          let fragment = this.createToolbarBtn(data);
-          item.appendChild(fragment);
+          let fragment = this.createToolbarBtn(data)
+          item.appendChild(fragment)
         }
-      });
+      })
     }
   }
 
   createToolbarBtn(data) {
-    let fragment = document.createDocumentFragment();
-    let divider = document.createElement("div");
-    divider.className = "protyle-toolbar__divider";
-    let btn = document.createElement("button");
-    btn.className = "protyle-toolbar__item b3-tooltips b3-tooltips__n";
-    btn.setAttribute("data-type", data.type);
-    btn.setAttribute("aria-label", data.label);
-    btn.innerHTML = config.icons[data.icon] || config.icons.common;
+    let fragment = document.createDocumentFragment()
+    let divider = document.createElement("div")
+    divider.className = "protyle-toolbar__divider"
+    let btn = document.createElement("button")
+    btn.className = "protyle-toolbar__item b3-tooltips b3-tooltips__n"
+    btn.setAttribute("data-type", data.type)
+    btn.setAttribute("aria-label", data.label)
+    btn.innerHTML = config.icons[data.icon] || config.icons.common
     btn.addEventListener("click", (e) => {
-      btn.parentElement.classList.add("fn__none"); //关闭 toolbar
-      data.func(e);
-    });
-    if (data.divider) fragment.appendChild(divider);
-    fragment.appendChild(btn);
-    return fragment;
+      btn.parentElement.classList.add("fn__none") //关闭 toolbar
+      data.func(e)
+    })
+    if (data.divider) fragment.appendChild(divider)
+    fragment.appendChild(btn)
+    return fragment
   }
 
   // 获取当前主题名称
   getThemeName() {
-    let themeStyle = document.querySelector("#themeStyle");
+    let themeStyle = document.querySelector("#themeStyle")
     if (themeStyle) {
-      let url = themeStyle.getAttribute("href").split("/");
-      return url[url.length - 2];
+      let url = themeStyle.getAttribute("href").split("/")
+      return url[url.length - 2]
     } else {
-      setTimeout(() => this.getThemeName(), 500);
+      setTimeout(() => this.getThemeName(), 500)
     }
   }
 
   // 插入样式表
   appendStyleSheet() {
-    let node = document.querySelector("#protyleHljsStyle");
+    let node = document.querySelector("#protyleHljsStyle")
     if (!node) {
       setTimeout(() => {
-        this.appendStyleSheet();
-      }, 500);
+        this.appendStyleSheet()
+      }, 500)
     } else {
-      let fragment = document.createDocumentFragment();
-      let css = document.createElement("link");
-      css.setAttribute("rel", "stylesheet");
-      css.setAttribute("type", "text/css");
+      let fragment = document.createDocumentFragment()
+      let css = document.createElement("link")
+      css.setAttribute("rel", "stylesheet")
+      css.setAttribute("type", "text/css")
       css.setAttribute(
         "href",
         "./appearance/themes/" + this.themeName + "/style/siyuan-utils.css"
-      );
-      fragment.appendChild(css);
-      document.head.insertBefore(fragment, node);
+      )
+      fragment.appendChild(css)
+      document.head.insertBefore(fragment, node)
     }
   }
 }
 
-new SiyuanPlugin();
+new SiyuanPlugin()
---
+++ js/libs/SearchBox.js
@@ -0,0 +1,240 @@
+import dropdownList from "./DropdownList.js"
+import { request } from "../utils/network.js"
+import config from "../config.js"
+
+class SearchBox {
+  constructor() {
+    this.searchBox = null
+    this.box = null
+    this.mask = null
+    this.isShow = false
+    this.actionName = ""
+    this.defaultPlaceHolder = "请输入命令"
+  }
+
+  handleKeyDown(e) {
+    // 移除 action 图标
+    if (
+      this.isShow &&
+      this.actionName &&
+      (e.key == "Delete" || e.key == "Backspace")
+    ) {
+      if (this.input.innerText == "") this.removeAction()
+    }
+
+    // esc 关闭搜索框
+    if (this.isShow && e.key == "Escape") {
+      this.showBox(false)
+    }
+
+    // 切换列表项
+    if (
+      this.isShow &&
+      (e.key == "ArrowUp" ||
+        e.key == "ArrowDown" ||
+        e.key == "Enter" ||
+        e.key == "Tab")
+    ) {
+      this.dropdownList.switchItems(e)
+    }
+
+    // 选择列表项
+    if (this.isShow && e.key == "Enter") {
+      if (this.list.style.opacity == 1) {
+        this.selectItem(this.dropdownList.selectItem())
+      } else {
+        this.handleInput(e)
+      }
+    }
+  }
+
+  create() {
+    this.searchBox = document.querySelector("#lz-search-box")
+    if (!this.searchBox) {
+      let fragement = document.createDocumentFragment()
+
+      this.box = document.createElement("div")
+      this.box.className = "box"
+      this.mask = document.createElement("div")
+      this.mask.className = "mask"
+      this.mask.addEventListener("click", () => {
+        this.showBox(false)
+      })
+
+      this.input = document.createElement("div")
+      this.input.className = "input"
+      this.input.setAttribute("contenteditable", true)
+      this.input.setAttribute("placeholder", this.defaultPlaceHolder)
+      this.input.setAttribute("spellcheck", false)
+
+      this.icon = document.createElement("div")
+      this.icon.className = "icon"
+
+      this.list = document.createElement("div")
+      this.list.className = "list"
+
+      this.box.appendChild(this.input)
+      this.box.appendChild(this.icon)
+      this.searchBox = document.createElement("div")
+      this.searchBox.id = "lz-search-box"
+      this.searchBox.appendChild(this.box)
+      this.searchBox.appendChild(this.list)
+      this.searchBox.appendChild(this.mask)
+      fragement.appendChild(this.searchBox)
+      document.body.append(fragement)
+
+      this.dropdownList = new dropdownList(
+        "#lz-search-box .list",
+        "#lz-search-box .list-item"
+      )
+    }
+  }
+
+  async selectItem(item) {
+    this.showLoading()
+    this.showList(false) //关闭列表项
+    window.getSelection().removeAllRanges() //移除输入框焦点
+
+    let id = item.getAttribute("data-id"),
+      pic = item.getAttribute("data-img"),
+      title = item.getAttribute("data-title"),
+      author = item.getAttribute("data-author"),
+      url = item.getAttribute("data-url"),
+      year = item.getAttribute("data-year")
+
+    let html = await request("https://book.douban.com/subject/" + id, "", "get") //访问豆瓣书目主页
+    // 正则匹配获取目录信息
+    let reg =
+      /<div class="indent" id="dir_\d+_full" style="display:none">([\s\S]*?)\(<a/
+    let result = html.match(reg),
+      dir = ""
+    if (result) {
+      dir = result[1].replace(/\s/g, "").replace(/<br\/>/g, "\n\n")
+    }
+    let now = new Date().toDateString()
+    // let content = `![img](${pic})\n\n**书目**：[${title}](${url})\n\n**作者**：${author} \n\n**出版年份**：${year}\n\n**标签**：\n\n**阅读日期**：${now}\n\n**在线阅读**：微信阅读\n\n### 推荐语\n\n\n\n### 阅读心得\n\n\n\n### 书摘\n\n\n\n### 知识应用\n\n\n\n### 相关阅读\n\n\n\n### 书籍目录\n\n\n\n${dir}`
+    const content = config.searchBox.template
+      .replace("{pic}", pic)
+      .replace("{title}", title)
+      .replace("{author}", author)
+      .replace("{url}", url)
+      .replace("{year}", year)
+      .replace("{dir}", dir)
+      .replace("{now}", now)
+
+    this.createDoc({ title: `《${title}》`, content: content })
+    this.showLoading(false)
+    this.showBox(false)
+    item.classList.remove("on")
+  }
+
+  /* 创建文档 */
+  createDoc(doc) {
+    let data = {
+      notebook: "20220317102842-0fpuxs6", //笔记本ID
+      path: "/读书笔记/" + doc.title,
+      markdown: doc.content,
+    }
+    request("/api/filetree/createDocWithMd", data).then((res) => {
+      if (res.data) window.open(`siyuan://blocks/${res.data}`)
+    })
+  }
+
+  showBox(show = true) {
+    if (show) {
+      this.isShow = true
+      this.searchBox.style.display = "block"
+      this.input.focus()
+    } else {
+      this.isShow = false
+      this.searchBox.style.display = "none"
+      this.input.innerText = ""
+      this.input.setAttribute("placeholder", this.defaultPlaceHolder)
+      this.list.innerHTML = ""
+      this.removeAction()
+    }
+  }
+
+  showLoading(show = true) {
+    if (show) {
+      this.icon.classList.add("loading")
+    } else {
+      this.icon.classList.remove("loading")
+    }
+  }
+
+  actionTrigger(e) {
+    // console.log(this.isShow);
+    if (this.isShow) {
+      let txt = this.input.innerText
+      if (!this.actionName && txt) {
+        if (txt.search(/^豆瓣\s/) == 0) {
+          this.createAction("豆瓣", "请输入书籍名称")
+        }
+
+        
+      }
+    }
+  }
+
+  handleInput(e) {
+    if (this.input && !this.input.innerText) this.showList(false) //输入框为空则收起列表项
+    // 屏蔽功能键
+    // let keys = ['ArrowDown','ArrowUp','ArrowLeft','ArrowRight','Escape','Enter','Tab']
+    // if(keys.indexOf(e.key) > -1) return
+
+    switch (this.actionName) {
+      case "豆瓣":
+        if (this.input.innerText) this.getBooks(this.input.innerText)
+        break
+
+      default:
+        break
+    }
+  }
+
+  getBooks(keyword) {
+    // console.log('访问豆瓣...');
+    let url = `https://book.douban.com/j/subject_suggest?q=${keyword}`
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.length > 0) {
+          this.dropdownList.createItems(res) //创建列表项
+          this.showList()
+        } else {
+          this.showList(false)
+        }
+      })
+  }
+
+  createAction(name, placeholder) {
+    this.actionName = name
+    placeholder = placeholder || this.defaultPlaceHolder
+    this.action = document.createElement("div")
+    this.action.className = "action"
+    this.action.innerText = name
+    this.input.innerText = ""
+    this.input.setAttribute("placeholder", placeholder)
+    this.box.insertBefore(this.action, this.input)
+  }
+
+  removeAction() {
+    if (this.action) this.action.remove()
+    this.actionName = ""
+    this.input.setAttribute("placeholder", this.defaultPlaceHolder)
+  }
+
+  showList(show = true) {
+    if (show) {
+      this.list.style.display = "block"
+      this.list.style.opacity = 1
+    } else {
+      this.list.style.display = "none"
+      this.list.style.opacity = 0
+    }
+    this.dropdownList.itemIndex = 0
+  }
+}
+
+export default SearchBox
